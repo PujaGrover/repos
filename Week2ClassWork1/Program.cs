2b6413f049fde81730b26a23609f1b3c4d5b9f2f
Puja Grover <puja_grover@yahoo.com>
2019-02-03 20:33:34 -0600

    Editting Main

--- Week2ClassWork1/Program.cs
+++ Week2ClassWork1/Program.cs
@@ -6,7 +6,7 @@ namespace Week2ClassWork1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!, CoderGirl is awesome");
             Console.ReadLine();
         }
     }
